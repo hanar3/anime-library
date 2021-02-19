2584cf19880b1b74669e891ef1420ecfd1419009
Vitor Moreno <vh.morenodasilva@gmail.com>
2021-02-19 16:50:48 -0300

    Added CreateAnimeService

---
+++ server/src/services/CreateAnimeService.ts
@@ -0,0 +1,20 @@
+import Anime from "../entity/Anime";
+import { getRepository } from "typeorm";
+
+interface ICreateAnime {
+  name: string;
+  englishName?: string;
+  description?: string;
+  japaneseName?: string;
+  episodes?: number;
+  status: string;
+}
+
+export default class AddAnimeService {
+  public async execute(data: ICreateAnime): Promise<Anime> {
+    const animeRepository = getRepository(Anime);
+    const anime = animeRepository.create(data);
+    await animeRepository.save(anime);
+    return anime;
+  }
+}
