c0aed3cfc5bcaef3b43f4716658da41e21869e3b
Vitor Moreno <vh.morenodasilva@gmail.com>
2021-02-22 18:52:46 -0300

    Added more fields to reviews tablea

---
+++ server/src/migration/1614028215490-MoreFieldsToReview.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class MoreFieldsToReview1614028215490 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumns("reviews", [
+      new TableColumn({
+        name: "status",
+        type: "varchar",
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: "watchedEpisodes",
+        type: "integer",
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: "createdAt",
+        type: "timestamp",
+        isNullable: false,
+      }),
+      new TableColumn({
+        name: "updatedAt",
+        type: "timestamp",
+        isNullable: false,
+      }),
+    ]);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumns("reviews", [
+      new TableColumn({
+        name: "status",
+        type: "varchar",
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: "watchedEpisodes",
+        type: "integer",
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: "createdAt",
+        type: "timestamp",
+        isNullable: false,
+      }),
+      new TableColumn({
+        name: "updatedAt",
+        type: "timestamp",
+        isNullable: false,
+      }),
+    ]);
+  }
+}
