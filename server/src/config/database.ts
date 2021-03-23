52b77b94292fd898e8c999e9735ff3172ab05667
Vitor Moreno <vh.morenodasilva@gmail.com>
2021-03-23 10:21:41 -0300

    Use env vars to connect to database

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,12 +1,11 @@
 import { ConnectionOptions } from "typeorm";
-import path from 'path';
 
 export default {
   type: "postgres",
-  host: "postgres",
+  host: process.env.POSTGRES_HOST,
   port: 5432,
   username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
-  database: "animelibdev",
+  database: process.env.POSTGRES_DATABASE,
   logging: false,
 } as ConnectionOptions;
